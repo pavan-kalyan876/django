a4270353d8ef1734d223ea4b935327ec3c633f02
pavansta <pavan.learn69@gmail>
2023-10-03 14:46:19 +0530

    created sample data in views and passed into a function where we have to display like in which html

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -2,7 +2,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+rooms = [
+    
+        {'id':1, 'name':'lets learn python!'},
+        {'id':2, 'name':'Design with me '},
+        {'id':3, 'name':'Frontend developer'},
+
+    
+]
 def home(request):
-    return  render(request,"home.html")
+    return  render(request,"home.html",{'rooms':rooms})
 def room(request):
     return render(request,'room.html')
