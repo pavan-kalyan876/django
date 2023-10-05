a1e4e09c664d37dbbe262924c289ac3c6d4743e9
pavansta <pavan.learn69@gmail>
2023-10-05 15:49:47 +0530

    created database in models in base app successfully migrated

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,4 +1,19 @@
 from django.db import models
-
+from django.db.models import Model
 # Create your models here.
 
+class Room(models.Model):
+    # host
+    # topic
+      name = models.name = models.CharField(max_length=250)
+      description = models.TextField(blank=True, null=True)
+    #participants = models
+
+      updated = models.DateTimeField(auto_now=True)
+
+    #create datetime fields in the database that are automatically updated to the current datetime.
+      created = models.DateTimeField(auto_now_add=True)
+
+      def __str__(self):
+        return self.name
+    
