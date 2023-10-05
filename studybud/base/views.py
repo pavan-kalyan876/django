c1391e42dcd0e4891278570eecf78aa34afd1104
pavansta <pavan.learn69@gmail>
2023-10-05 17:35:20 +0530

    find connected to admin to views ,the content in admin displayed in wed

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,25 +1,27 @@
-
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 
 rooms = [
-    
-        {'id':1, 'name':'lets learn python!'},
-        {'id':2, 'name':'Design with me '},
-        {'id':3, 'name':'Frontend developer'},
-
-    
+    {"id": 1, "name": "lets learn python!"},
+    {"id": 2, "name": "Design with me "},
+    {"id": 3, "name": "Frontend developer"},
 ]
+
+#rooms in admin.py
 def home(request):
-    context = {'rooms':rooms}
-    return  render(request,"base/home.html",context)
-#accessing pk from urls.py  here where we have to display
-def room(request,pk):
+    rooms = Room.objects.all()
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
+
+
+# accessing pk from urls.py  here where we have to display
+def room(request, pk):
     room = None
     for i in rooms:
-        if i['id'] == int(pk):
+        if i["id"] == int(pk):
             room = i
-    context = {'room':room}
+    context = {"room": room}
 
-    return render(request,'base/room.html',context)
+    return render(request, "base/room.html", context)
