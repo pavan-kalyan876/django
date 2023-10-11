d728692126544a19e3f375c6f08280d6da9eed2c
pavansta <pavan.learn69@gmail>
2023-10-12 00:17:58 +0530

    in crud operation update operations done after saving redirecting to home page in views.py

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -40,8 +40,13 @@ def createRoom(request):
     return render(request, "base/room_form.html", context)
 
 
-def updateRoom(request,pk):
+def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
-    form = RoomForm(instance=room)   
-    context = {'form':form}
-    return render(request,'base/room_form.html',context)
+    form = RoomForm(instance=room)
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
